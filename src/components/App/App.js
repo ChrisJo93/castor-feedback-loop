59e107403ec15c8006f4f0d959613a3ea6003347
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-07 19:02:03 -0600

    imported hash router, made header a component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+// CUSTOM COMPONENTS
+import Header from '../header/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
+        {/* Cleaning up app by making header a component */}
+        <Header />
+        <br />
+        <div></div>
       </div>
     );
   }
