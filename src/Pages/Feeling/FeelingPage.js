695567c1f84b26fb98d82705ef1b27aed7f892ba
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-07 21:48:32 -0600

    feelings has input and submit functionality. Sending to store

--- src/Pages/Feeling/FeelingPage.js
+++ src/Pages/Feeling/FeelingPage.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Feeling extends Component {
+  state = {
+    form: '',
+  };
+
+  updateFeels = (event) => {
+    this.setState({
+      form: event.target.value,
+    });
+  };
+
+  submitFeels = (event) => {
+    console.log(this.state.form);
+  };
+
   render() {
     return (
       <div>
@@ -9,8 +23,13 @@ class Feeling extends Component {
           <h2>How Are you Feeling?</h2>
         </header>
         <div>
-          <input type="number" placeholder="Enter a number" />
-          <button>Submit</button>
+          <input
+            type="number"
+            placeholder="Enter a number"
+            onChange={this.updateFeels}
+            value={this.state.form}
+          />
+          <button onClick={this.submitFeels}>Submit</button>
         </div>
       </div>
     );
