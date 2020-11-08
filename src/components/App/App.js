984976bdb773ac174946c32e546cb3dfda6dc9ef
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-07 19:27:59 -0600

    added button links to each page, adding routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,12 +9,29 @@ import Header from '../header/header';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        {/* Cleaning up app by making header a component */}
-        <Header />
-        <br />
-        <div></div>
-      </div>
+      <Router>
+        <div className="App">
+          {/* Cleaning up app by making header a component */}
+          <Header />
+          <nav>
+            <button>
+              <Link to="/feeling">How Do you Feel?</Link>
+            </button>
+            <button>
+              <Link to="/understanding">Understand the Material?</Link>
+            </button>
+            <button>
+              <Link to="/support">Feel Supported?</Link>
+            </button>
+            <button>
+              <Link to="/comments">Leave a Comment</Link>
+            </button>
+          </nav>
+
+          {/* <Router exact path="/feeling" component={Feeling} /> */}
+          <br />
+        </div>
+      </Router>
     );
   }
 }
